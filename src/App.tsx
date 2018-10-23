07636043a9baa4c9af986269c8f7da42afb6f7bd
Charlon Palacay <charlon@gmail.com>
2018-10-23 10:39:07 -0700

    Adding base app common boilerplate to mimic myplan.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Boilerplate from './boilerplate/Boilerplate';
+// import Boilerplate from './boilerplate/Boilerplate';
+import Common from './common/Common';
 import Courses from './myplan/courses/'
 import Home from './myplan/home/'
 import Programs from './myplan/programs/'
 import Registration from './myplan/registration/'
 
 const App = () => (
-  <Boilerplate>
+  <Common>
     <Switch>
       <Route exact={true} path="/" component={Home} />
       <Route path="/programs" component={Programs} />
       <Route path="/courses" component={Courses} />
       <Route path="/registration" component={Registration} />
     </Switch>
-  </Boilerplate>
+  </Common>
 );
 
 export default App;
---
+++ src/common/Common.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap-reboot.css'
+import './common.css';
+
+class Common extends React.Component<{}> {
+
+
+  public render() {
+    return (
+
+      <section>
+
+        <header role="banner" className="com-header">
+          <div className="com-thin-bar">
+            <div className="com-container">
+              <div className="com-links">
+                <ul>
+                  <li>MyUW</li>
+                  <li>UW.edu</li>
+                  <li>W</li>
+                  <li><i className="fas fa-question-circle" /></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="com-masthead">
+            <div className="com-container">
+              <div className="com-title-mobile">
+                <button className="com-title-button" id="nav_button">
+                  <span className="fa-stack fa-2x com-title-button-icon" aria-hidden="true">
+                    <i className="far fa-square fa-stack-2x" />
+                    <i className="fa fa-bars fa-stack-1x" />
+                  </span>
+                </button>
+                <span>MyPlan</span>
+              </div>
+              <div className="com-title-desktop">MyPlan</div>
+            </div>
+          </div>
+        </header>
+
+
+        <div className="com-body">
+          <div className="com-container">
+
+            <main role="main" className="com-main">
+              {this.props.children}
+            </main>
+
+          </div>
+        </div>
+
+      </section>
+
+    );
+  }
+
+}
+
+export default Common;
